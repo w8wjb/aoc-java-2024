19c6b162bc75ae755b5158422124fa3ba26fc806
Weston Bustraan <wbustraan@gmail.com>
2024-12-02 12:16:18 -0500

    Removed the commented code that I abandoned

--- src/main/java/org/tot/aoc/Day2.java
+++ src/main/java/org/tot/aoc/Day2.java
@@ -95,59 +95,6 @@ public class Day2 {
 
             }
 
-//
-//            int direction = 0;
-//            int unsafeLevels = 0;
-//            boolean skipUsed = false;
-//            int lastLevel = levels[0];
-//
-//            for (int i = 1; i < levels.length; i++) {
-//                int level = levels[i];
-//
-//                int currDirection = (level > lastLevel) ? 1 : -1;
-//                if (direction == 0) {
-//                    direction = currDirection;
-//                }
-//
-//                if (!isSafe(lastLevel, level, direction)) {
-//                    if (!skipUsed) {
-//                        if (i > 1 && isSafe(levels[i - 2], level, direction)) {
-//                            // Would it work if I skipped the one prior?
-////                            System.out.print("Skipped index ");
-////                            System.out.println(i-1);
-//                            lastLevel = levels[i - 2];
-//                            skipUsed = true;
-//                            continue;
-//                        } else if (i < (levels.length - 1) && isSafe(levels[i - 1], levels[i + 1], direction)) {
-//                            // What if I skip the current one, would that fix it?
-//                            lastLevel = levels[i - 1];
-//                            skipUsed = true;
-////                            System.out.print("Skipped index ");
-////                            System.out.println(i);
-//                            continue;
-//                        } else if (i == levels.length - 1) {
-//                            skipUsed = true;
-//                            System.out.println(line);
-//                            // Edge case: bad level is the last one in the list
-//                            continue;
-//                        }
-//                    }
-//                    unsafeLevels++;
-//                }
-//                lastLevel = level;
-//            }
-//            if (unsafeLevels == 0) {
-//                System.out.print("Safe");
-//                if (skipUsed) {
-//                    System.out.print(" * :");
-//                } else {
-//                    System.out.print("   :");
-//                }
-//                safeCount++;
-//            } else {
-//                System.out.print("Unsafe :");
-//            }
-//            System.out.println(line);
         }
 
 
