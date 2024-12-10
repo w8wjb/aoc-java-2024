c16237e17e7a05522bd241185a8f35df8260bb48
Weston Bustraan <wbustraan@gmail.com>
2024-12-10 14:10:00 -0500

    Day 5 - added teaching comments

--- src/main/java/org/tot/aoc/Day5.java
+++ src/main/java/org/tot/aoc/Day5.java
@@ -11,6 +11,8 @@ import java.util.stream.Collectors;
 
 public class Day5 {
 
+    // This is a Set that will contain the pairs of numbers that define the ordering rules.
+    // It's here, outside the other methods because it's needed by the inner class
     Set<Pair<Integer, Integer>> orderingRules = new HashSet<>();
 
 
@@ -23,16 +25,22 @@ public class Day5 {
 
         for (String line : input) {
 
+            // When we encounter the blank line, we have to change the way the input is parsed.
             if (line.isBlank()) {
                 rulesSection = false;
                 continue;
             }
 
             if (rulesSection) {
+                // Each line in the rules section is two numbers separated by a pipe |
+                // First, split the line into 2 parts
                 String[] parts = line.split("\\|");
+                // Using the Commons-Lang Pair class here
                 Pair<Integer, Integer> pair = new ImmutablePair<>(Integer.valueOf(parts[0]), Integer.valueOf(parts[1]));
                 orderingRules.add(pair);
+
             } else {
+                // For the pages list, split the line by the commas, convert each item to and int, and then collect to a list
                 List<Integer> pages = Arrays.stream(line.split(","))
                         .map(Integer::valueOf)
                         .collect(Collectors.toList());
@@ -43,16 +51,34 @@ public class Day5 {
         return updates;
     }
 
+
+    /**
+     * This is the magic for this puzzle. In Java, you can provide a custom Comparator to be used when sorting a list.
+     * This class uses the provide rules to determine the ordering of the pages.
+     */
     private class PageComparator implements Comparator<Integer> {
 
+        /**
+         * Taking advantage of the fact that Comparators are objects on their own and can have side effects.
+         * Defaults to true, because we assume, at the beginning, that the pages are in order until we find otherwise.
+         */
         public boolean orderedCorrectly = true;
 
+        /**
+         *
+         * @param left the first object to be compared.
+         * @param right the second object to be compared.
+         * @return L &lt; R = -1, L &gt; R = 1, and 0 for no change
+         */
         @Override
         public int compare(Integer left, Integer right) {
 
+            // Try both combinations
             var lr = new ImmutablePair<>(left, right);
             var rl = new ImmutablePair<>(right, left);
+
             if (orderingRules.contains(lr)) {
+                // If we find a match in the rules, but backwards, we can tell that this pair is out of order.
                 orderedCorrectly = false;
                 return -1;
             } else if (orderingRules.contains(rl)) {
@@ -71,11 +97,16 @@ public class Day5 {
         var updates = parseInput(input);
 
         for (List<Integer> update : updates) {
+
+            // Creating a new comparator per trip through the loop because we want orderedCorrectly to reset to true
             var comparator = new PageComparator();
+
+            // This is where most of the work is done
             update.sort(comparator);
 
 
             if (comparator.orderedCorrectly) {
+                // Find the number in the middle of the update list
                 int middleIndex = update.size() / 2;
                 total += update.get(middleIndex);
             }
@@ -96,6 +127,7 @@ public class Day5 {
             update.sort(comparator);
 
 
+            // The difference between part 1 and part 2 is literally just the not (!) sign below
             if (!comparator.orderedCorrectly) {
                 int middleIndex = update.size() / 2;
                 total += update.get(middleIndex);
