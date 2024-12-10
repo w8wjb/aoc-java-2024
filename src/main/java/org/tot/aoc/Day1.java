99d0e3fec55b6edd6319f93c9c9751372e9f63bd
Weston Bustraan <wbustraan@gmail.com>
2024-12-10 08:52:49 -0500

    Added teaching comments to day 1 & 2

--- src/main/java/org/tot/aoc/Day1.java
+++ src/main/java/org/tot/aoc/Day1.java
@@ -6,26 +6,40 @@ public class Day1 {
 
     public int solvePuzzle1(List<String> input) {
 
+        // Create 2 lists to hold the numbers, left column and right column
         List<Integer> leftCol = new ArrayList<>();
         List<Integer> rightCol = new ArrayList<>();
 
+        // Parse each line
         for (String line : input) {
+            // Rather than splitting strings like many do, Java has a Scanner class that already understands whitespace
+            // and helper functions to convert from strings to ints
             var scan = new Scanner(line);
+            // Grab the next int and add it to the list
             leftCol.add(scan.nextInt());
             rightCol.add(scan.nextInt());
         }
 
+        // This is the main trick to this puzzle; if you sort both lists from smallest to largest, the pairs will
+        // line up like they're supposed to.
         Collections.sort(leftCol);
         Collections.sort(rightCol);
 
 
+        // Keep a running total
         int total = 0;
 
+        // Using the index of just one column here
         for (int i = 0; i < leftCol.size(); i++) {
+
+            // Grab the item at the same index in both lists
             int left = leftCol.get(i);
             int right = rightCol.get(i);
 
-            int diff = Math.max(left, right) - Math.min(left, right);
+            // Find the difference between the two columns
+            // They could be in reverse order, where right is larger than left, resulting in a negative number, so we
+            // fix that by taking the absolute value.
+            int diff = Math.abs(left - right);
             total += diff;
         }
 
@@ -35,20 +49,33 @@ public class Day1 {
 
     public int solvePuzzle2(List<String> input) {
 
+        // This time, I'm only making a list for the left column.
         List<Integer> leftCol = new ArrayList<>();
+
+        // This is a map where the key is a unique number in the right column and the value is the number of times
+        // it occurs.
         Map<Integer, Integer> counts = new HashMap<>();
 
+        // Parse each line
         for (String line : input) {
             var scan = new Scanner(line);
+
+            // Add the first number to the left column
             leftCol.add(scan.nextInt());
 
             int right = scan.nextInt();
+
+            // This is using a feature of Map where, if you lookup a key (first arg) and it's not there, it will
+            // return the default (second arg). This is to avoid getting a null and having to handle it
             int count = counts.getOrDefault(right, 0);
+            // Now, add 1 to the count and update the map
             counts.put(right, count + 1);
         }
 
+        // Keep a running total
         int total = 0;
         for (int left : leftCol) {
+            // For each number in the left column, find the count from the right
             int count = counts.getOrDefault(left, 0);
             total += (left * count);
         }
--- src/main/java/org/tot/aoc/Day2.java
+++ src/main/java/org/tot/aoc/Day2.java
@@ -5,14 +5,30 @@ import java.util.*;
 public class Day2 {
 
 
+    /**
+     * Applies the rules to see if a pair of levels are safe:
+     * <ul>
+     * <li>The levels are either all increasing or all decreasing.</li>
+     * <li>Any two adjacent levels differ by at least one and at most three.</li>
+     * </ul>
+     *
+     * @param last      first level in a pair
+     * @param next      second level in a pair
+     * @param direction the overall direction the sequence is going. 1 means increasing, -1 means decreasing
+     * @return true if the levels are safe
+     */
     private boolean isSafe(int last, int next, int direction) {
+        // Find the absolute distance between the two levels.
         int distance = Math.abs(last - next);
 
+        // "Any two adjacent levels differ by at least one and at most three."
         if (distance < 1 || distance > 3) {
             return false;
         }
 
+        // Calculate if the next one is increasing or decreasing
         int currDirection = (next > last) ? 1 : -1;
+        // The current direction should match the overall direction
         return currDirection == direction;
     }
 
@@ -22,25 +38,41 @@ public class Day2 {
         int safeCount = 0;
 
         for (String line : input) {
+
+            // Using a Scanner again, here, instead of splitting the String. Seems less messy to me.
             Scanner scan = new Scanner(line);
 
+            // Keep track of the overall direction.
+            // It starts at 0 because we don't know until the first pair what the direction will be
             int direction = 0;
+
+            // Count how many unsafe levels we found
             int unsafeLevels = 0;
+
+            // Grab the first level
             int lastLevel = scan.nextInt();
 
             while (scan.hasNext()) {
+                // We're going to be comparing each level to the previous level in the list
                 int level = scan.nextInt();
 
-                int currDirection = (level > lastLevel) ? 1 : -1;
+                // If the direction hasn't been determined yet, we need to set it
                 if (direction == 0) {
-                    direction = currDirection;
+                    direction = (level > lastLevel) ? 1 : -1;
+                    ;
                 }
 
+                // Check to see if this pair of levels is safe
                 if (!isSafe(lastLevel, level, direction)) {
+                    // If not, increment the count
                     unsafeLevels++;
                 }
+
+                // The current level becomes the last level
                 lastLevel = level;
             }
+
+            // If no unsafe levels were found, add 1 to the safe count.
             if (unsafeLevels == 0) {
                 safeCount++;
             }
@@ -50,22 +82,43 @@ public class Day2 {
         return safeCount;
     }
 
+    /**
+     * Applies the safe/unsafe rules to an array of levels.
+     *
+     * <p>This is the same logic from part 1, just put in a method of its own so it can be called over and over</p>
+     *
+     * @param levels array of levels
+     * @return true if the levels are safe
+     */
     private boolean isSafe(int[] levels) {
-        int direction = 0;
-        int unsafeLevels = 0;
-        for (int i = 1; i < levels.length; i++) {
-            int level = levels[i];
-            int lastLevel = levels[i-1];
 
-            int currDirection = (level > lastLevel) ? 1 : -1;
+        // Overall direction, defaults to zero because we don't know what it is yet
+        int direction = 0;
+        // Count of unsafe levels.
+        int unsafeLevels = 0;
+
+        // Loop over the levels, using a "for-i" style loop because I want the index number.
+        // Notice that 'i' starts at 1. This is because I'll be accessing the previous item with "i - 1"
+        // and it would crash on the first element, where i = 0.
+        for (int i = 1; i < levels.length; i++) {
+
+            // Unlike above, where the levels come from a Scanner, we can access the levels using indexes
+            int level = levels[i];
+            // Get the preceding item in the list
+            int lastLevel = levels[i - 1];
+
+            // If the direction hasn't been determined yet, we need to set it
             if (direction == 0) {
-                direction = currDirection;
+                direction = (level > lastLevel) ? 1 : -1;;
             }
 
+            // Check to see if this pair of levels is safe
             if (!isSafe(lastLevel, level, direction)) {
                 unsafeLevels++;
             }
         }
+
+        // Return true if there are no unsafe levels
         return unsafeLevels == 0;
     }
 
@@ -75,20 +128,40 @@ public class Day2 {
 
         for (String line : input) {
 
+            // This is using Java's streams feature. This basically:
+            // 1. Splits the string by whitespace using a regular expression, "\s+". This results in an array: String[]
+            // 2. For each item in the String[], convert it to an int using `Integer.parseInt()`
+            // 3. Convert this stream back to an array of int[]
             int[] levels = Arrays.stream(line.split("\\s+"))
                     .mapToInt(Integer::parseInt)
                     .toArray();
 
+            // I'm using arrays instead of Lists here for efficiency.
+            // I wanted to use some of Java's array copying functions
+            // You could probably use a List just as well
+
+            // Check the safety of the initial set of levels. If it's safe already, we can just count it and move on
             if (isSafe(levels)) {
                 safeCount++;
-            } else {
-                for (int i = 0; i < levels.length; i++) {
-                    int[] omitList = new int[levels.length - 1];
-                    System.arraycopy(levels, 0, omitList, 0, i);
-                    System.arraycopy(levels, i+1, omitList, i, omitList.length - i);
 
+            } else {
+                // If we got here, it means that there is something in the list that makes it unsafe.
+                // The approach from here is to go item by item in the array, take it out of the list, and check it
+                for (int i = 0; i < levels.length; i++) {
+
+                    // Make a new array that is 1 size smaller.
+                    int[] omitList = new int[levels.length - 1];
+
+                    // Copy items form the beginning of the source list to the target list, up to the item we want to omit
+                    System.arraycopy(levels, 0, omitList, 0, i);
+                    // Copy items from the end of the source list to the target list, after item we want to omit
+                    System.arraycopy(levels, i + 1, omitList, i, omitList.length - i);
+
+                    // Now, with this new, smaller list (minus the item) check it for safety.
                     if (isSafe(omitList)) {
                         safeCount++;
+                        // This break is important because, once we've found an item that would make the list safe,
+                        // we don't need to keep looking. The break stops the inner for loop, but not the outer line loop
                         break;
                     }
                 }
